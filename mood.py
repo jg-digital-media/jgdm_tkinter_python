a869e5caabaa01894b3cd14d57021b30d4116589
Jonathan Grieve <mail@jonniegrieve.co.uk>
2026-04-03 20:32:40 +0100

    create app skeleton and title frame

--- mood.py
+++ mood.py
@@ -1,4 +1,28 @@
-# mood.py - add simple message to terminal to test the app
+import tkinter as tk
 
-message = "Hello World!"
-print(message)
+root = tk.Tk()
+root.title("Mood Tracker")
+root.geometry("500x600")
+
+# Main sections
+title_frame = tk.Frame(root)
+mood_frame = tk.Frame(root)
+action_frame = tk.Frame(root)
+history_frame = tk.Frame(root)
+summary_frame = tk.Frame(root)
+
+title_frame.pack(fill="x", pady=10)
+mood_frame.pack(fill="x", pady=10)
+action_frame.pack(fill="x", pady=10)
+history_frame.pack(fill="both", expand=True, pady=10)
+summary_frame.pack(fill="x", pady=10)
+
+
+tk.Label(
+    title_frame,
+    text="Tkinter Mood Tracker",
+    font=("Arial", 16)
+).pack()
+
+# Run the app
+root.mainloop()
